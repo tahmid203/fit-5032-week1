c8bf4b5fe3fd8d747c78bdd2f7ebf50c553f7b2b
Tahmid <makh0002@student.monash.edu>
2025-08-08 18:47:24 +1000

    init

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import MyName from './components/MyName.vue'
+import JSON from './components/JSON.vue'
+</script>
+
+<template>
+  <div class="full-app">
+    <JSON />
+  </div>
+</template>
+
+<style scoped>
+.full-app {
+  width: 100vw;
+}
+
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
